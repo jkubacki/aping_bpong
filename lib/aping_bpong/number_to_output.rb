6e3b34c3c73f6d8ca4d6354712f3e15e674a082d
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-24 14:19:48 +0200

    Implement ApingBpong::NumberToOutput

--- lib/aping_bpong/number_to_output.rb
+++ lib/aping_bpong/number_to_output.rb
@@ -1,9 +1,21 @@
 module ApingBpong
   # For multiples of three, it returns 'APin'
   # For the multiples of five, it returns 'BPong'
+  # For numbers which are multiples of both three and five, it returns 'APingBPong'
   # It returns the number for all other numbers
   class NumberToOutput
+    MOD_3_OUTPUT = 'APing'.freeze
+    MOD_5_OUTPUT = 'BPong'.freeze
+    MOD_3_AND_MOD_5_OUTPUT = "#{MOD_3_OUTPUT}#{MOD_5_OUTPUT}".freeze
+
     def call(number:)
+      mod_three = (number % 3).zero?
+      mod_five = (number % 5).zero?
+
+      return MOD_3_AND_MOD_5_OUTPUT if mod_three && mod_five
+      return MOD_3_OUTPUT if mod_three
+      return MOD_5_OUTPUT if mod_five
+
       number
     end
 
